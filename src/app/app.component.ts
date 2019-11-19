92d77db96868756794bfa064f8b1e8321c7667e0
unknown <cesarocampo9@hotmail.com>
2019-11-19 10:19:47 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'TODOMOTO MOTOPARTS';
+
+  employees = [
+    {'name': 'Carlos', possition: 'manager' },
+    {'name': 'Juan', possition: 'services' },
+    {'name': 'Carlos', possition: 'sales' }
+  ];
+}
